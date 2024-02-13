00c0cd29ffff0a14c06b65e2b0597dd5fb6378a0
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2024-02-13 14:29:51 +0000

    Update 20240212_UPDATED_INDICATORS.R

--- .github/workflows/20240212_UPDATED_INDICATORS.R
+++ .github/workflows/20240212_UPDATED_INDICATORS.R
@@ -1,6 +1,7 @@
 
-
-read.xlsx("C:/Users/flanagam/Documents/HOUSECOMMENCEMNTS.xlsx")
+library(rvest)
+library(readxl)
+library(emayili)
 
 URL_COMMENCEMENTS<-"https://www.gov.ie/en/publication/a5cb1-construction-activity-starts/"
 HREF<-read_html(URL_COMMENCEMENTS)  %>% html_nodes(xpath='//*[@id="download_link_245151"]')  %>% html_attr('href')
@@ -18,8 +19,9 @@ Current_Comm<-read_excel("Commencements_Current.xlsx")
 UPDATED_INDEX<-NA
 
 
-if(na.omit(Current_Comm[ncol(Current_Comm)])[1,1] == na.omit(NEW_DATA[ncol(NEW_DATA)])[1,1]){
-  UPDATED_INDEX<-paste0("* ",print("Housing Commencements, Availble: "),HREF)
+
+if(na.omit(Current_Comm[ncol(Current_Comm)])[1,1] != na.omit(NEW_DATA[ncol(NEW_DATA)])[1,1]){
+  UPDATED_INDEX<-paste0("* ",print("Housing Commencements, Availble:", ))
 
 }
 
@@ -30,7 +32,7 @@ UPDATED_release<-na.omit(UPDATED_INDEX)
 UPDATED_release_1<-if(paste(UPDATED_release[1])== "NA"){
   paste0("")
 } else {
-  paste0(UPDATED_release)
+  paste0(UPDATED_release[1])
 }
 
 
@@ -47,7 +49,7 @@ UPDATED_EMAIL<-emayili::envelope(
   from="mflanagan201@gmail.com",
   subject = "Updated Indicator!"
 ) %>%
-  emayili::render(' <span class="text-center" style="color:#A3915E"> <left> <font size="4"> *Hi, It seems like the following indicator has just been updated!* </font> </left> </span>
+  emayili::render(' <span class="text-center" style="color:#A3915E"> <left> <font size="4"> <font face="Arial"> *Hi, It seems like the following indicator has just been updated!* </font> </left> </span>
 
                          
                        {{UPDATED_release_1}}   
