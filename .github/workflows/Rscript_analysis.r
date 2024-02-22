88b7e9f8b486d40119700a0e857f712486c50859
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2024-02-22 15:37:11 +0000

    Update Rscript_analysis.r

--- .github/workflows/Rscript_analysis.r
+++ .github/workflows/Rscript_analysis.r
@@ -766,7 +766,7 @@ smtp <- emayili::server(host = "smtp.gmail.com",
 
 Body_weekly_email<-emayili::envelope(
   to=c("michael.flanagan@finance.gov.ie"
-  ),bcc=c("michael.flanagan@finance.gov.ie","Oisin.Tarrant@finance.gov.ie","Joanne.Mulholland@finance.gov.ie","Brendan.O'Connor@finance.gov.ie","Hannah.Cousins@finance.gov.ie","Fionn.Roche@finance.gov.ie","Luke.Rehill@finance.gov.ie","Eimear.Flynn@finance.gov.ie", "Sorcha.O'Connor@finance.gov.ie","harry.morris@finance.gov.ie","David.Hughes@finance.gov.ie","Pascal.McMenamy@finance.gov.ie"),
+  ),bcc=c("michael.flanagan@finance.gov.ie","Oisin.Tarrant@finance.gov.ie","Joanne.Mulholland@finance.gov.ie","Brendan.O'Connor@finance.gov.ie","Hannah.Cousins@finance.gov.ie","Fionn.Roche@finance.gov.ie","Luke.Rehill@finance.gov.ie","Eimear.Flynn@finance.gov.ie", "Sorcha.O'Connor@finance.gov.ie","harry.morris@finance.gov.ie","David.Hughes@finance.gov.ie","Pascal.McMenamy@finance.gov.ie","Patrick.OBrien@finance.gov.ie"),
   
   from="mflanagan201@gmail.com",
   subject = "Weekly Economic Calendar"
