72d38326a3c2b7c486c326ee6a82e155881004b5
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2025-02-19 13:38:40 +0000

    Update Macro_Releases.R

--- .github/workflows/Macro_Releases.R
+++ .github/workflows/Macro_Releases.R
@@ -32,18 +32,7 @@ smtp <- server(host = "smtp.gmail.com",
                username = "mflanagan201@gmail.com",
                password = "ddauvuifpknvsobo")
 
-
-Body_INFLATION<-emayili::envelope(
-    from = "mflanagan201@gmail.com",
-    to=c("michael.flanagan@finance.gov.ie"),
-    subject = "[Auto-Email] Inflation Release"
-  ) %>%
-    emayili::render(".github/workflows/2024_March_Inflation_efficent_v3.Rmd")  
-
-  #smtp(Body_INFLATION)
-
-
-
+
 
 for(i in 1:nrow(CALENDAR_ALL_short)){
 if(Sys.Date()==as.Date(CALENDAR_ALL_short$DTSTART[i],format="%Y-%m-%d") && CALENDAR_ALL_short$SUMMARY[i] %like any% c("%Consumer Price Index%")){
@@ -51,18 +40,20 @@ if(Sys.Date()==as.Date(CALENDAR_ALL_short$DTSTART[i],format="%Y-%m-%d") && CALEN
     Body_INFLATION<-emayili::envelope(
     from = "mflanagan201@gmail.com",
     to=c("michael.flanagan@finance.gov.ie"),
-bcc=c("ian.power@finance.gov.ie","harry.morris@finance.gov.ie"),
+bcc=c("ian.power@finance.gov.ie","eamonn.sweeney@finance.gov.ie"),
     subject = "[Auto-Email] Inflation Release"
   ) %>%
     emayili::render(".github/workflows/2024_March_Inflation_efficent_v3.Rmd")  
 
-    #smtp(Body_INFLATION)
+    smtp(Body_INFLATION)
   
     } else { 
     
     print("Don't run code")
   }
 }
+
+
 
 for(i in 1:nrow(CALENDAR_ALL_short)){
 if(Sys.Date()+1==as.Date(CALENDAR_ALL_short$DTSTART[i],format="%Y-%m-%d") && CALENDAR_ALL_short$SUMMARY[i] %like any% c("%Consumer Price Index%")){
@@ -74,7 +65,7 @@ if(Sys.Date()+1==as.Date(CALENDAR_ALL_short$DTSTART[i],format="%Y-%m-%d") && CAL
   ) %>%
     emayili::render(".github/workflows/2024_March_Inflation_efficent_v3.Rmd")  
 
-   #smtp(Body_INFLATION)
+   smtp(Body_INFLATION)
   
     } else { 
     
@@ -83,21 +74,7 @@ if(Sys.Date()+1==as.Date(CALENDAR_ALL_short$DTSTART[i],format="%Y-%m-%d") && CAL
 }
 
 
-
-#print("running code from now")
-        Body_EXTERNAL_TRADE<-emayili::envelope(
-          from = "mflanagan201@gmail.com",
-         to=c("michael.flanagan@finance.gov.ie"),
-subject = "External Trade Release!"
-      ) %>%
-        # Render R Markdown from a file.
-        emayili::render(".github/workflows/Monthly_External_Trade_Beta_5.Rmd")
-    
-     # smtp(Body_EXTERNAL_TRADE)  
-    
-    
-
-
+  
 
 for(i in 1:nrow(CALENDAR_ALL_short)){
   if(Sys.Date()==as.Date(CALENDAR_ALL_short$DTSTART[i],format="%Y-%m-%d") && CALENDAR_ALL_short$SUMMARY[i] %like any% c("%Goods Exports and Imports%")){
@@ -122,39 +99,3 @@ bcc=c("Ian.Power@finance.gov.ie","Oisin.Tarrant@finance.gov.ie","Daire.DeHora@fi
 
 
 
-WEEK<-seq(from=as.Date("02/12/2024", format=c('%d/%m/%Y')),to=as.Date("31/12/2025", format=c('%d/%m/%Y')), by="week")
-if(Sys.Date() %like any% WEEK){
-
-  MACRO_TABLE<-emayili::envelope(
-          from = "mflanagan201@gmail.com",
-        to=c("michael.flanagan@finance.gov.ie"),
-    bcc=c("Ian.Power@finance.gov.ie","David.Hughes@finance.gov.ie","Joanne.Mulholland@finance.gov.ie","Pascal.McMenamy@finance.gov.ie","Patrick.OBrien@finance.gov.ie","Patrick.Wolohan@finance.gov.ie","mark.hennessy@finance.gov.ie"), 
-      subject = "[Auto-Email] Macroeconomic Tables"
-    ) %>%
-       # Render R Markdown from a file.
-  #    emayili::render(".github/workflows/macro_tables.Rmd")
-    
-     #smtp(MACRO_TABLE)  
-
-} else {
-  print("nothing to send")
-}
-
-
-
- # MACRO_TABLE<-emayili::envelope(
-          from = "mflanagan201@gmail.com",
-        to=c("michael.flanagan@finance.gov.ie"),
-          subject = "[Auto-Email] Macroeconomic Tables"
-    ) %>%
-       # Render R Markdown from a file.
-    #  emayili::render(".github/workflows/#macro_tables.Rmd")
-    
-   
-
-
-tryCatch({
-  smtp(MACRO_TABLE)
-}, error = function(e) {
-  message("SMTP error: ", e$message)
-})
