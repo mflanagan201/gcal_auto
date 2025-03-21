ab358de0796fbe20f431ce05bd8ebfa0d40d00c2
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2025-03-21 07:51:33 +0000

    Update Rscript_analysis.r

--- .github/workflows/Rscript_analysis.r
+++ .github/workflows/Rscript_analysis.r
@@ -765,10 +765,10 @@ smtp <- emayili::server(host = "smtp.gmail.com",
 
 
 Body_weekly_email<-emayili::envelope(
-  to=c("michael.flanagan@finance.gov.ie"
+ from="mflanagan201@gmail.com",
+ to=c("michael.flanagan@finance.gov.ie"
   ),bcc=c("Ian.Power@finance.gov.ie","Eamonn.Sweeney@finance.gov.ie","michael.flanagan@finance.gov.ie","Oisin.Tarrant@finance.gov.ie","Joanne.Mulholland@finance.gov.ie","Hannah.Cousins@finance.gov.ie","Fionn.Roche@finance.gov.ie","Luke.Rehill@finance.gov.ie","Eimear.Flynn@finance.gov.ie", "Sorcha.O'Connor@finance.gov.ie","harry.morris@finance.gov.ie","David.Hughes@finance.gov.ie","Pascal.McMenamy@finance.gov.ie","Patrick.OBrien@finance.gov.ie","Harry.Williamson@finance.gov.ie","Eoin.OFlaithearta@finance.gov.ie"),
   
-  from="mflanagan201@gmail.com",
   subject = "Weekly Economic Calendar"
 ) %>%
   emayili::render(' <span class="text-center" style="color:#205E55"> <left> <font size="4"> *Hi, The following indicators will be released next week.* </font> </left> </span>
