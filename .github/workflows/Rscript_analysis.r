a02c26f64dadf8fafc8b61f048606bfe06949be5
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2025-04-24 12:30:48 +0200

    Update Rscript_analysis.r

--- .github/workflows/Rscript_analysis.r
+++ .github/workflows/Rscript_analysis.r
@@ -73,7 +73,7 @@ RELEASE_PMI<-data.frame(matrix(NA,nrow=nrow(PMI_XTS),ncol=3))
 
 
 for(i in 1:nrow(PMI_XTS)){
-if(grepl("Ireland Manufacturing PMI|Ireland Services PMI|Ireland Construction PMI", PMI_XTS$Release[i], ignore.case = TRUE)){
+if(grepl("Ireland Manufacturing PMI|Ireland Services PMI| Ireland Construction PMI", PMI_XTS$Release[i], ignore.case = TRUE)){
   RELEASE_PMI[i,]<-PMI_XTS[i,]
  } else {
   RELEASE_PMI[i,] <- NA
