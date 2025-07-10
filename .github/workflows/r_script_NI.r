247ab443ed40b077fed83cdc1d95b892bfdd85a6
mflanagan201 <125045921+mflanagan201@users.noreply.github.com>
2025-07-10 10:18:18 +0200

    Update r_script_NI.r

--- .github/workflows/r_script_NI.r
+++ .github/workflows/r_script_NI.r
@@ -13,6 +13,7 @@ library(curl)
 library(jsonlite)
 library(xts)
 library(dplyr)
+library(httr)
 
 URL<-c("https://www.nisra.gov.uk/upcoming-releases/date/2025?page=0")
 NI_df<- read_html(URL)%>% html_nodes("ul") %>% html_text() %>% stringr::str_split("\n") %>% .[[5]]%>%  trimws() %>% data.frame()  %>% .[!.[1:nrow(.),]=="",] %>% data.frame()  
